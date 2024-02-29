45946438848882fb79f2d5a4092061b6bfc665ce
HichamHanane <hichamhanane2000@gmail.com>
2024-02-29 19:50:54 +0100

    add blog app DB

---
+++ Blog app database/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const router = require('./Routes/Routes');
+
+require('./DB/ConnectDB');
+
+
+
+app.set('view-engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+    
+app.use('/',router)
+
+
+
+app.listen(2500,()=>{
+    console.log("server is runing in port 2500....");
+});
